b7caf296758bfee63dea5f68c34b39f5d857c646
Nikita <mstruhalski@tut.by>
2020-12-24 00:18:28 +0300

    Two tests that count average marks were added
    When I was making this lab, I created new project and copied all files from lab7. So all changes I've made today in lab7 I didn't transferred to this lab

--- test.cpp
+++ test.cpp
@@ -238,3 +238,72 @@ TEST(StudentAfterSecondSession, Output)
     EXPECT_EQ(output, "Name:  Petya  Group:  2  Course:  1Marks  after  first  session:"
                       "  10  10  11  10  Marks  after  second  session:  10  10  9  10  10  ");
 }
+
+TEST(AverageMark, ForAGroup)
+{
+    int *massive1 = new int[4] {10, 10, 11, 10};
+    int *massive2 = new int[4] {9, 10, 9, 9};
+    int *massive3 = new int[5] {9, 9, 10, 9, 10};
+    int *massive4 = new int[5] {10, 9, 9, 9, 9};
+
+    Student student1("Ivan Ivanov", 1, 3);
+    StudentAfterFirstSession student1_1(student1, massive1);
+    StudentAfterSecondSession student1_2(student1_1, massive3);
+
+    Student student2("Piotr Petrov", 1, 4);
+    StudentAfterFirstSession student2_1(student2, massive1);
+    StudentAfterSecondSession student2_2(student2_1, massive3);
+
+    Student student3("Misha Mishin", 1, 4);
+    StudentAfterFirstSession student3_1(student3, massive2);
+    StudentAfterSecondSession student3_2(student3_1, massive4);
+
+    std::vector<Student*> all_students;
+    all_students.push_back(&student1);
+    all_students.push_back(&student1_1);
+    all_students.push_back(&student1_2);
+    all_students.push_back(&student2);
+    all_students.push_back(&student2_1);
+    all_students.push_back(&student2_2);
+    all_students.push_back(&student3);
+    all_students.push_back(&student3_1);
+    all_students.push_back(&student3_2);
+
+
+    EXPECT_EQ(AverageMarkForAGroup(all_students, 1, 4, 2), 9.5);
+    EXPECT_EQ(AverageMarkForAGroup(all_students, 1, 4, 1), 9.75);
+    EXPECT_EQ(AverageMarkForAGroup(all_students, 1, 3, 0), 0);
+}
+
+TEST(AverageMark, ForAllStudents)
+{
+    int *massive1 = new int[4] {10, 10, 11, 10};
+    int *massive2 = new int[4] {9, 10, 9, 9};
+    int *massive3 = new int[5] {9, 9, 10, 9, 10};
+    int *massive4 = new int[5] {10, 9, 9, 9, 9};
+
+    Student student1("Ivan Ivanov", 1, 3);
+    StudentAfterFirstSession student1_1(student1, massive1);
+    StudentAfterSecondSession student1_2(student1_1, massive3);
+
+    Student student2("Piotr Petrov", 1, 4);
+    StudentAfterFirstSession student2_1(student2, massive1);
+    StudentAfterSecondSession student2_2(student2_1, massive3);
+
+    Student student3("Misha Mishin", 1, 4);
+    StudentAfterFirstSession student3_1(student3, massive2);
+    StudentAfterSecondSession student3_2(student3_1, massive4);
+
+    std::vector<Student*> all_students;
+    all_students.push_back(&student1);
+    all_students.push_back(&student1_1);
+    all_students.push_back(&student1_2);
+    all_students.push_back(&student2);
+    all_students.push_back(&student2_1);
+    all_students.push_back(&student2_2);
+    all_students.push_back(&student3);
+    all_students.push_back(&student3_1);
+    all_students.push_back(&student3_2);
+    double mark = (double) ((10.25 + ((double)88 / 9)) * 2 + 9.25 + ((double)83 / 9)) / 6;
+    EXPECT_EQ(AverageMarkForAllStudents(all_students), mark);
+}
