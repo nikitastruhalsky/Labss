d6af2fb343b90ad8be8b3d810589f28a07cbf44c
Nikita <mstruhalski@tut.by>
2021-03-29 09:02:43 +0300

    assembler_lab5 done

---
+++ lab5.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+extern "C"
+{
+    int __cdecl Arcsin (float x);
+    float __cdecl Task2 (float x, float eps);
+}
+
+int main()
+{
+    /*float x, eps, result = 0;
+    std::cout << "Enter x: ";
+    std::cin >> x;
+    std::cout << Arcsin(x) << std::endl;
+    std::cout << "Enter an epsilon: ";
+    std::cin >> eps;*/
+    std::cout << Task2(2, 0.2) << std::endl;
+    return 0;
+}
